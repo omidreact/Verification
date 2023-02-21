988cdc3feacb6ed7e94783f696d1d59f26f966fb
omid ramezani <116552870+omidreact@users.noreply.github.com>
2023-02-21 11:25:51 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+import Verification from './Screens/Verification/Verification';
+
+
+const style = { flex: 1, backgroundColor: '#fff' };
+
+const App = () => (
+  <ScrollView >
+    <Verification />
+
+  </ScrollView>
+);
+
+export default App;
---
+++ Screens/Users.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+function Users() {
+
+    return (
+      <View style={styles.firstsection}>
+        <Text>product screen</Text>
+      </View>
+    );
+
+}
+const styles = StyleSheet.create({
+  firstsection: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:"lightblue"
+  },
+});
+export default Users;
